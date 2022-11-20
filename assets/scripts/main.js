6b14a50899d43d1a0c6ab6ad38729b3d393d7777
Андрей Шушунов <a.shushunov@runetsoft.ru>
2022-11-21 00:31:39 +0500

    chore [#1] create first-screen for desctop

---
+++ assets/scripts/main.js
@@ -0,0 +1,25 @@
+function initializeFullPageScript() {
+  new fullpage('#fullpage', {
+    licenseKey: 'gplv3-license',
+    autoScrolling: true,
+    navigation: false,
+    anchors: ['first-screen','portfolio','services','about', 'contacts'],
+    navigationTooltips: ['s-1','s-2','s-3','s-4', 's-5'],
+  })
+}
+
+
+function activateFullMenu() {
+  let $menuButton = document.querySelector('.menu')
+  let $body = document.querySelector('body')
+
+  $menuButton.addEventListener('click', () => {
+    $body.classList.toggle('active-full-menu')
+  })
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  initializeFullPageScript()
+  activateFullMenu()
+})
